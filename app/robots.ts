56c72b383c913b10de5b0feb7f5aaa4423b842f2
malsgier-oss <m.alsgier@gmail.com>
2026-02-06 16:30:17 +0200

    Add dynamic export configuration to robots.ts for static rendering

--- app/robots.ts
+++ app/robots.ts
@@ -1,4 +1,7 @@
+export const dynamic = "force-static";
+
 import type { MetadataRoute } from "next";
+
 export default function robots(): MetadataRoute.Robots {
   return { rules: [{ userAgent: "*", allow: "/" }], sitemap: "https://certistech.com/sitemap.xml" };
 }
